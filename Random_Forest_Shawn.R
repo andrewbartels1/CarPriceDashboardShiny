876b420fafed93c08cff394d999aef844fee6f81
thesazzu <shawnazzu@gmail.com>
2022-04-20 11:49:38 -0700

    Basic Decision Tree Regression (Random Forest) Model using randomForest. Will build more complex version with Caret. Please see notes in file

---
+++ Random_Forest_Shawn.R
@@ -0,0 +1,51 @@
+
+#title: "Random_Forest_Shawn"
+#author: Shawn
+
+  
+  #### Clear Environment
+rm(list = ls())
+
+
+#### Load Libraries
+library(tidyverse)
+library(dplyr)
+library(readxl) 
+library(stringr) # string formatting
+library(RSQLite) # to connect to SQlite database
+library(roxygen2) # For Function Documentation: ctrl + option + shift + r
+
+#random forest library, will also try with Caret Package
+library(randomForest)
+
+#set path to the database
+db_path <- "C:/Users/shawn/MGT 6203 Project/CraigslistCarsClean.sqlite3"
+
+conn <- dbConnect(RSQLite::SQLite(), db_path)
+
+#dbDisconnect(conn)
+
+dbListTables(conn)
+
+#Grab the Ford Table
+ford <- dbGetQuery(conn, "SELECT * FROM Ford")
+
+#Take a gander at the Ford Table
+head(ford)
+summary(ford)
+
+
+#simple randomForest
+#basic decsion tree
+model = randomForest(price ~ ., data = ford, 
+                     do.trace = TRUE)
+
+#Note that the above simple randomForest will run 500 Trees, so I implemented do.trace = TRUE
+# to see the progress
+
+#Observe output
+model
+
+summary(model)
+
+
